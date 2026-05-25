d902ce7ead9a27a7095896df58be517e7de3a46e
sidharthv-maker <siddharth08vutukuru@gmail.com>
2026-05-25 17:24:16 +0530

    Add the generating loop and fix bugs

--- model.py
+++ model.py
@@ -8,26 +8,26 @@ from torch.utils.data import DataLoader, TensorDataset
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.emb = nn.Embedding(128,32)
+        self.emb = nn.Embedding(28,32)
         self.mod = nn.LSTM(32,128, batch_first = True)
         self.layer = nn.Linear(128,28)
-    def forward(self, x, hidden = True):
+    def forward(self, x, hidden = None):
         x = self.emb(x)
         out, hidden = self.mod(x,hidden)
         return self.layer(out), hidden
     
 data = pd.read_csv("data/yob2010.txt", header = None, names=["name", "gender", "freq"])
-lst = data["name"].string.lower().tolist()
+lst = data["name"].str.lower().tolist()
 
-chars = sorted("".join(lst))
-chars = ["<start>, <end>"] + chars
+chars = sorted(set("".join(lst)))
+chars = ["<start>", "<end>"] + chars
 
 cha = {}
 idx = {}
 
 for i, ch in enumerate(chars):
     cha[ch] = i
-for ch, i in cha.item():
+for ch, i in cha.items():
     idx[i] = ch
 
 sequence = []
@@ -40,7 +40,7 @@ loader = DataLoader(dset, 32, True)
 
 model = Model()
 optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
-lossfn = torch.CrossEntropyLoss(ignore_index = 0)
+lossfn = nn.CrossEntropyLoss(ignore_index = 0)
 
 for epochs in range(200):
     model.train()
@@ -49,9 +49,29 @@ for epochs in range(200):
         target = batch[0][:, 1:]
         optimizer.zero_grad()
         out, _ = model(x)
-        out.view(-1, 28)
-        target.reshape(-1)
-        loss = lossfn(target)
+        out = out.view(-1, 28)
+        target = target.reshape(-1)
+        loss = lossfn(out, target)
         loss.backward()
         optimizer.step()
 
+num = int(input("Hello, welcome to the Name Generator. Enter the number of names you want to generate: "))
+length = int(input("What is the maximum length of the name you want to generate: "))
+
+for _ in range(num):
+    model.eval()
+    token = cha["<start>"]
+    name = ""
+    hidden = None
+    for i in range(length):
+        inp = torch.tensor([[token]])
+        out, hidden = model(inp, hidden)
+        hidden = (hidden[0].detach(), hidden[1].detach())
+        prob = F.softmax(out.squeeze()/0.8, 0)
+        token = torch.multinomial(prob, 1).item()
+        ch = idx[token]
+        if ch == "<end>":
+            break
+        name += ch
+    
+    print(name)
